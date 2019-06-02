b0af08c4699c8ec4b0223a9c80fab64791285c49
jrb <jrb.youssef@gmail.com>
2019-06-02 01:10:31 +0100

    add vue2-editor and some node modules to app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,11 @@ window.Vue = require('vue');
 import BootstrapVue from 'bootstrap-vue';
 
 import CompleteProfile from './components/Profile/CompleteProfile.vue';
+import NewQuestion from './components/Question/NewQuestion';
 
 Vue.use(BootstrapVue);
 
 const app = new Vue({
     el: '#app',
-    components: { CompleteProfile }
+    components: { CompleteProfile, NewQuestion }
 });
